a8255fdb32c1c2f451e4697d8247e7760788439a
Chandan <chandelc4@gmail.com>
2022-12-02 20:24:53 -0500

    barebones code + start of unit testing

--- Library/Library/Book.h
+++ Library/Library/Book.h
@@ -5,15 +5,13 @@ class Book {
 private:
 	std::string b_name;
 	std::string b_author;
-	std::string b_picPath;
-	int b_publishDate;
+	std::tm b_publishDate;
 	int b_size;
 public:
-	Book(std::string name, std::string author, std::string picPath, int publishDate, int size) 
-		: b_name(name), b_author(author), b_picPath(picPath), b_publishDate(publishDate), b_size(size){}
+	Book(std::string name, std::string author, std::tm publishDate, int size) 
+		: b_name(name), b_author(author), b_publishDate(publishDate), b_size(size){}
 	std::string getName() { return b_name; }
 	std::string getAuthor() { return b_author; }
-	std::string getPicPath() { return b_picPath; }
-	int getPublishDate() { return b_publishDate; }
+	std::tm getPublishDate() { return b_publishDate; }
 	int getSize() { return b_size; }
 };
--- Library/Library/LibraryIndex.h
+++ Library/Library/LibraryIndex.h
@@ -2,20 +2,23 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <unordered_set>
 #include <unordered_map>
 #include "Book.h"
+#include <ctime>
 using namespace std;
 class LibraryIndex{
 private:
-	vector<Book> Library;
+	unordered_map<std::string, Book> Library;
 	priority_queue<string> sortByName;
-	priority_queue<pair<int, string>>sortByDate;
+	priority_queue<pair<tm, string>>sortByDate;
 	priority_queue<pair<int, string>>sortBySize;
 	unordered_map<string, vector<string>>searchByAuthor;
-	unordered_map<string, Book*>searchByName;
 public:
-	bool addBook() {}
-	bool deleteBook() {}
-	void printStats() {}
+	bool addBook(std::string name, std::string author, int size, int publishDate, int publishMonth, int publishYear) {
+	}
+	bool deleteBook(std::string name) {
+	}
+	std::string printStats() {}
 };
 
